407df8368d69d821c1f06a19440331597ac43d19
shivam <shivam@DESKTOP-BT1O840>
2023-09-14 09:06:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+    public string FullCarName
+    {
+        get { return $"{Make} {Model} {Year}"; }
+    }
+
+    public Car(string make, string model, int year)
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car myCar = new Car("Toyota", "Camry", 2022);
+
+        string carName = myCar.FullCarName;
+        Console.WriteLine("My car: " + carName);
+
+        Console.ReadLine();
+    }
+}
